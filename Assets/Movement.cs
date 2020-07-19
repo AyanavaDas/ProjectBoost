5c2f0c0f6acd2a63b0b7765bbc452f46bb14f870
AyanavaDas <51167125+AyanavaDas@users.noreply.github.com>
2020-07-19 19:48:07 +0530

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rocket;
+    AudioSource thruster;
  
     void Start()
     {
         rocket = GetComponent<Rigidbody>();
+        thruster = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -26,6 +28,9 @@ public class Movement : MonoBehaviour
             //relative force because we want to use local coordinates// 
             //vector3 up adds force in the y direction//
             rocket.AddRelativeForce(Vector3.up);
+            if(!thruster.isPlaying)
+                thruster.Play(0);
+
         }
         //can thrust and rotate
         if(Input.GetKey(KeyCode.A))
@@ -33,11 +38,15 @@ public class Movement : MonoBehaviour
             //vector3 forward is +z direction
             //rotates according to left thumb rule
             transform.Rotate(Vector3.forward);
+            if (thruster.isPlaying)
+                thruster.Stop();
         }
         else if(Input.GetKey(KeyCode.D))
         {
             //rotates according to left thumb rule
             transform.Rotate(-(Vector3.forward));
+            if (thruster.isPlaying)
+                thruster.Stop();
         }
     }
 }
